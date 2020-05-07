f82706828fa4028c7de0f88a2b9ffe3e2c314aa1
Aunger-ops <1796245767@qq.com>
2020-05-07 12:20:32 +0800

    Site updated: 2020-05-07 12:20:30

--- projects/js/init.js
+++ projects/js/init.js
@@ -1,4 +1,4 @@
-Vue.component('card-item', {
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="https://cdn.jsdelivr.net/npm/aplayer@1.10/dist/APlayer.min.css"><script src="https://cdn.jsdelivr.net/npm/aplayer@1.10/dist/APlayer.min.js" class="aplayer-secondary-script-marker"></script>Vue.component('card-item', {
     template: '#card-item',
     props: ['title', 'desc', 'icon', 'time', 'url', 'color'],
 })
